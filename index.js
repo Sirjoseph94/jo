63d0cf1a6cd3a46dd37aff1552086822252e764e
Dec <dec@GentleStorms-Air.local>
2022-06-26 23:27:03 +0100

    inital commit of personal portfolio site

---
+++ index.js
@@ -0,0 +1,8 @@
+const stack = "<span class='highlight'>NodeJs, ReactJs, Typescript</span> and <span class='highlight'>Laravel</span>"
+const typed = new Typed('.sentence', {
+  strings: [`I'm <b>Joseph Ero</b>, a software engineer. <br> My current tech stack includes <br> ${stack}.`],
+    typeSpeed: 30,
+    showCursor: false,
+    startDelay: 100,
+  fadeOut: true
+});
